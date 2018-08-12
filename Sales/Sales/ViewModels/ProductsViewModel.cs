7a7be0c51ddf52ed30660d6dee3bf7a747af5ec8
brandonvg89 <brandonvg89@gmail.com>
2018-08-11 21:36:12 -0500

    Changes in Application
    The changes in application are viewer product list.
    
    Generate models and controller of api project
    
    Implementations of internazionalitation for spanish, english and portugues languages
    
    Implementations of file resources

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,76 @@
+﻿namespace Sales.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using Common.Models;
+    using GalaSoft.MvvmLight.Command;
+    using Sales.Helpers;
+    using Services;
+    using Xamarin.Forms;
+
+    public class ProductsViewModel : BaseViewModel
+    {
+        private ApiService apiService;
+
+        private ObservableCollection<Product> products;
+
+        private bool isRefreshing;
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            this.IsRefreshing = true;
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, connection.Message, Languages.Accept);
+                return;
+            }
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var response = await this.apiService.GetList<Product>(url, "/api", "/Products");
+
+            if (!response.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+
+            this.IsRefreshing = false;
+        }
+
+        public ICommand RefreshCommand {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
+        }
+    }
+}
+
